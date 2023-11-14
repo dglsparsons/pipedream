4e7e8931bafadc2be870e9fbb003fc3408e153af
Douglas Parsons <dglsparsons@gmail.com>
2023-11-14 16:48:39 +0000

    Initial setup

---
+++ main.tf
@@ -0,0 +1,118 @@
+terraform {
+  required_version = "1.6.2"
+  backend "s3" {
+    bucket         = "dglsparsons-terraform-state"
+    key            = "pipedream.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.25"
+    }
+  }
+}
+
+data "terraform_remote_state" "platform" {
+  backend   = "s3"
+  workspace = "default"
+
+  config = {
+    bucket         = "dglsparsons-terraform-state"
+    key            = "platform-core.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+locals {
+  aws_account = "doug"
+  prefix      = "pipedream"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${data.terraform_remote_state.platform.outputs.account_ids[local.aws_account]}:role/Deployer"
+  }
+}
+
+resource "aws_dynamodb_table" "workflows" {
+  name         = "${local.prefix}-workflows"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  range_key    = "created_at"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "S"
+  }
+
+  attribute {
+    name = "status"
+    type = "S"
+  }
+
+  attribute {
+    name = "due_to_run"
+    type = "S"
+  }
+
+  global_secondary_index {
+    hash_key        = "status"
+    range_key       = "due_to_run"
+    name            = "workflows_by_status"
+    projection_type = "ALL"
+  }
+}
+
+data "aws_iam_policy_document" "workflows_dynamodb" {
+  statement {
+    actions = [
+      "dynamodb:Query",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:UpdateItem",
+      "dynamodb:ConditionCheckItem",
+    ]
+    resources = [
+      aws_dynamodb_table.workflows.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "workflows_dynamodb" {
+  name   = "${local.prefix}-workflows-dynamodb"
+  policy = data.aws_iam_policy_document.workflows_dynamodb.json
+}
+
+resource "aws_iam_user" "pipedream" {
+  name          = "${local.prefix}-api"
+  force_destroy = true
+}
+
+resource "aws_iam_user_policy_attachment" "workflows_dynamodb" {
+  user       = aws_iam_user.pipedream.name
+  policy_arn = aws_iam_policy.workflows_dynamodb.arn
+}
+
+resource "aws_iam_access_key" "pipedream" {
+  user    = aws_iam_user.pipedream.name
+  pgp_key = "keybase:dgls"
+}
+
+output "access_key" {
+  value = aws_iam_access_key.pipedream.id
+}
+
+output "encypted_secret_access_key" {
+  value = aws_iam_access_key.pipedream.encrypted_secret
+}
---
+++ src/app.rs
@@ -0,0 +1,53 @@
+use crate::error_template::{AppError, ErrorTemplate};
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    provide_meta_context();
+    view! {
+        <Stylesheet id="leptos" href="/pkg/tailwind.css"/>
+        <Stylesheet id="leptos" href="/pkg/pipedream.css"/>
+        <Title text="Welcome to Leptos"/>
+        <Router fallback=|| {
+            let mut outside_errors = Errors::default();
+            outside_errors.insert_with_default_key(AppError::NotFound);
+            view! {
+                <ErrorTemplate outside_errors/>
+            }
+            .into_view()
+        }>
+            <main>
+                <Routes>
+                    <Route path="" view=HomePage/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <Title text="Leptos + Tailwindcss"/>
+        <main>
+            <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
+                <div class="flex flex-row-reverse flex-wrap m-auto">
+                    <button on:click=move |_| set_count.update(|count| *count += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                        "+"
+                    </button>
+                    <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
+                        {count}
+                    </button>
+                    <button on:click=move |_| set_count.update(|count| *count -= 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                        "-"
+                    </button>
+                </div>
+            </div>
+        </main>
+    }
+}
